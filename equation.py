87a02a2cae480451d4fffc31e7839bfdf751bda3
Bartlomiej JANIK <bjanik@e2r4p8.42.fr>
2019-03-26 20:09:25 +0100

    Need to fix key error issue and add some tests

--- equation.py
+++ equation.py
@@ -51,13 +51,27 @@ class Equation:
 			tokens.append(Token(value, kind, coef, power))
 		return tokens
 
+	def parse_equation(self):
+		for token_list in [self.tokens_left, self.tokens_right]:
+			prev_token = None
+			for token in token_list:
+				if prev_token is not None:
+					if (token.kind in ['NUMBER', 'UNKNOWN'] and prev_token.kind in ['NUMBER', 'UNKNOWN']) or token.kind == prev_token.kind:
+						error("Parsing error")
+					if prev_token.kind == 'OP' and prev_token.value == '-':
+						token.coef *= -1
+						prev_token.value = '+'
+				prev_token = token
+			if prev_token.kind == "OP":
+				error("Parsing error")
+
 	def reduce_equation(self):
 		for power in self.powers_right.keys():
 			coef = self.powers_left.get(power)
 			if coef is None:
 				self.powers_left[power] = self.powers_right[power] * -1
 			else:
-				self.powers_left[power] -= coef
+				self.powers_left[power] -= self.powers_right[power] 
 
 	def print_reduced_equation(self):
 		for power, coef in sorted(self.powers_left.items()):
@@ -69,11 +83,32 @@ class Equation:
 				print("{}*X^{}".format(coef, power), end="")
 		print(" = 0")
 
+	def create_dict(self, stack):
+		dico = {}
+		for token in stack:
+			if dico.get(token.power) is None:
+				dico[token.power] = token.coef
+			else:
+				dico[token.power] += token.coef
+		for key in list(dico.keys()):
+			if dico[key] == 0:
+				del dico[key]
+		return dico
+		
 	def solve_equation(self):
-		a = self.powers_left.get('2', 0)
-		b = self.powers_left.get('1', 0)
-		c = self.powers_left.get('0', 0)
+		if float(max(self.powers_left.keys())) > 2:
+			error("The polynomial degree is stricly greater than 2, I can't solve")
+		a = self.powers_left.get('2.0', 0)
+		b = self.powers_left.get('1.0', 0)
+		c = self.powers_left.get('0.0', 0)
 		delta = b**2 - 4 * a * c
-		if delta == 0:
-			sol = -1 * (b / (2 * a))
-
+		if a:
+			if delta == 0:
+				x1 = -1 * (b / (2 * a))
+				print("Disciminant is 0, unique solution is\n{}".format(x1))
+			elif delta > 0:
+				x1 = (-1 * b - delta**0.5) / (2 * a)
+				x2 = (-1 * b + delta * 0.5) / (2 * a)
+				print("Disciminant is strictly positive, the two solutions are\n{}\n{}".format(x1, x2))
+			else:
+				print("Complex solution exists")
--- main.py
+++ main.py
@@ -1,43 +1,69 @@
-#!/usr/local/bin/python3
+#!/usr/bin/python3
 import re
 import sys
 
 from equation import Equation
+from token import Token
 from utils import error
 
 def main():
 	if len(sys.argv) == 1:
 		error("Missing input")
 	eq = Equation(sys.argv[1])
-	# for token in eq.tokens_left:
-	# 	print(token.kind, token.value)
-	# for token in eq.tokens_right:
-	# 	print(token.kind, token.value)
-	for token_list in [eq.tokens_left, eq.tokens_right]:
-		prev_token = None
-		for token in token_list:
-			if prev_token is not None:
-				if (token.kind in ['NUMBER', 'UNKNOWN'] and prev_token.kind in ['NUMBER', 'UNKNOWN']) or token.kind == prev_token.kind:
-					error("Parsing error")
-				if prev_token.kind == 'OP' and prev_token.value == '-':
-					token.coef *= -1
-					prev_token.value = '+'
-			prev_token = token
-		if prev_token.kind == "OP":
-			error("Parsing error")
-	for token in eq.tokens_left:
-		print(token.value, token.coef, token.power)
+	eq.parse_equation()
+	output_stack_left = create_rpn(eq.tokens_left)
+	output_stack_right = create_rpn(eq.tokens_right)
+	for token in output_stack_left:
+		print(token.value, end=" ")
+	left_stack = resolve_rpn(output_stack_left)
+	right_stack = resolve_rpn(output_stack_right)
+	eq.powers_right = eq.create_dict(right_stack)
+	eq.powers_left = eq.create_dict(left_stack)
+	eq.reduce_equation()
+	print(eq.powers_right)
+	print(eq.powers_left)
+	eq.solve_equation()
 
-def rpn(eq):
+def create_rpn(tokens):
 	op_precedence = {'+': 0, '*': 1}
 	output_stack = []
 	op_stack = []
-	for token in eq.tokens_left:
+	for token in tokens:
 		if token.kind == 'OP':
-			if len(op_stack) > 0 and op_stack[len(op_stack) - 1]
+			if len(op_stack) > 0 and op_precedence[op_stack[len(op_stack) - 1].value] > op_precedence[token.value]:
+				while (len(op_stack) != 0):
+					output_stack.append(op_stack.pop())
 			op_stack.append(token)
 		elif token.kind in ['NUMBER', 'UNKNOWN']:
 			output_stack.append(token)
+	while (len(op_stack) != 0):
+		output_stack.append(op_stack.pop())
+	return output_stack
+
+def resolve_rpn(input_stack):
+	stack = []
+	for token in input_stack:
+		if token.kind in ['NUMBER', 'UNKNOWN']:
+			stack.append(token)
+		if token.value == '*':
+			op1 = stack.pop()
+			op2 = stack.pop()
+			kind = 'UNKNOWN' if op1.kind == 'UNKNOWN' or op2.kind == 'UNKNOWN' else 'NUMBER'
+			coef = op1.coef * op2.coef
+			power = op1.power + op2.power if coef != 0 else 0 
+			tok = Token("", kind, coef, power)
+			stack.append(tok)
+		elif token.value == '+':
+			op1 = stack.pop()
+			op2 = stack.pop()
+			if op1.kind == op2.kind and op1.power == op2.power:
+				coef = op1.coef + op2.coef
+				tok = Token("", op1.kind, coef, power)
+				stack.append(tok)
+			else:
+				stack.append(op2)
+				stack.append(op1)
+	return stack
 
 if __name__ == "__main__":
 	main()
